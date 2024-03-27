d6b8bba1ac814c1241c4afac623e468a37b5a7d2
KBirk1s <KBirk1s@capetigers.com>
2024-03-27 10:34:36 -0500

    second day

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,36 @@
-const data = document.getElementsByClassName("data");
 const display = document.getElementById("displaySelection");
+const enter = document.getElementById("enter");
+const add = document.getElementById("addOption");
+const pool = document.getElementById("pool"); 
+    const data = new Array(document.getElementsByClassName("data"));
+enter.onclick = startSelection;
+add.onclick = addOption;
 
-data.addEventListener('keyup', (e) => {
-    createOptions(data.value);
-    if(e.key === "Enter") {
-        selectRandom();
+
+function startSelection() {
+    createTable();
+    console.log(data)
+    random();
+}
+
+function createTable() {
+    let table = document.createElement("table");
+
+    for (var i = 0; i < data.length; i++) {
+        table.innerHTML = "<tr><td>"+ data[i].value +"</td></tr>";
     }
-});
 
-function createOptions(input) {
+    display.appendChild(table);
+}
 
+
+function random() {
+
+}
+
+function addOption() {
+    const inputBox = document.createElement("input");
+    inputBox.classList.add("data");
+    inputBox.classList.add("col-3");
+    pool.insertBefore(inputBox, add);
 }
