6e2bb7aaf9585e64a87cd45a27693589a8a42a2d
Kamden <birkkamden@gmail.com>
2024-03-29 22:53:58 -0500

    MVP achieved

--- javascript/script.js
+++ javascript/script.js
@@ -2,35 +2,38 @@ const display = document.getElementById("displaySelection");
 const enter = document.getElementById("enter");
 const add = document.getElementById("addOption");
 const pool = document.getElementById("pool"); 
-    const data = new Array(document.getElementsByClassName("data"));
+const data = document.getElementsByClassName("data");
 enter.onclick = startSelection;
 add.onclick = addOption;
 
-
 function startSelection() {
     createTable();
-    console.log(data)
     random();
+    pool.innerHTML = "";
 }
 
 function createTable() {
     let table = document.createElement("table");
 
+    display.innerHTML = "";
+
     for (var i = 0; i < data.length; i++) {
-        table.innerHTML = "<tr><td>"+ data[i].value +"</td></tr>";
+        table.innerHTML += "<tr><td class='item'>"+ data[i].value +"</td></tr>";
+        data[i].value = "";
     }
 
     display.appendChild(table);
 }
 
-
 function random() {
-
+    const options = document.querySelectorAll(".item");
+    options[Math.floor(Math.random()*options.length)].classList.add("select");
 }
 
 function addOption() {
     const inputBox = document.createElement("input");
+    const brick = document.getElementById("brick");
     inputBox.classList.add("data");
     inputBox.classList.add("col-3");
-    pool.insertBefore(inputBox, add);
+    pool.insertBefore(inputBox, brick);
 }
